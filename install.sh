b773fe1351554b85d004d7ef4632e32048d93c7b
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2022-03-17 17:48:31 +0000

    :tada: first co

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd `dirname $0`
+
+nerd_fonts() {
+  git clone --branch=master --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+  cd nerd-fonts
+  ./install.sh $1  # "Source" to install Sauce Code Nerd Font
+  cd ..
+  rm -rf nerd-fonts
+}
+
+starship() {
+    curl -sS https://starship.rs/install.sh | sh
+}
+
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+
+    # シンボリックリンクを貼る
+    ln -snfv ${PWD}/"$f" ~/
+done
