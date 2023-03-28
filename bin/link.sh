8f057c0c40798c5d43f3977b2a8b0bac6fb18dea
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2023-03-28 15:47:25 +0900

    :bug: /usr/bin/bash -> /bin/bash

--- bin/link.sh
+++ bin/link.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/bash -eu
+#!/bin/bash -eu
 
 BASEDIR=$(pwd)
 cd $BASEDIR
 
 for dotfile in $BASEDIR/.??* ; do
     [[ "$dotfile" == "${BASEDIR}/.git" ]] && continue
+    [[ "$dotfile" == "${BASEDIR}/.gitignore" ]] && continue
     [[ "$dotfile" == "${BASEDIR}/.github" ]] && continue
     [[ "$dotfile" == "${BASEDIR}/.DS_Store" ]] && continue
 
